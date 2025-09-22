909f527649ac0ce4274dc5e87753e5af5e2b57ec
Alex Dev <alexanderokoji@gmail.com>
2025-09-22 09:54:48 +0100

    fix(server): remove wildcard app.options to resolve path-to-regexp error on Render

--- server/index.js
+++ server/index.js
@@ -34,7 +34,6 @@ app.use(cors({
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
 }));
-app.options('*', cors());
 app.use(express.json());
 
 const DATA_DIR = path.join(__dirname, 'data');
